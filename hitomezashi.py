d4ec5938361ec184bfa9cd48589cbb5fe73479c5
rhelmstedter <70656244+rhelmstedter@users.noreply.github.com>
2020-10-20 14:46:00 -0700

    Update hitomezashi.py

--- hitomezashi.py
+++ hitomezashi.py
@@ -1,52 +1,53 @@
-#! python3
+#!python3
 import random
 
 HEIGHT = 30
 WIDTH = 30
 
-vgrid = []
-vodd = []
-veven = []
-hgrid = []
-hodd = []
-heven = []
+vs_grid = []
+vs_oddrow = []
+vs_evenrow = []
+hs_grid = []
+hs_oddrow = []
+hs_evenrow = []
 
-# contruct veritcal stiches
-for x in range(WIDTH):
+# contruct veritcal stiches randomly for first row and opposite for second row.
+for stitch in range(WIDTH):
     if random.randint(0, 1) == 1:
-        vodd.append('|')
-        veven.append(' ')
+        vs_oddrow.append('|')
+        vs_evenrow.append(' ')
     else:
-        vodd.append(' ')
-        veven.append('|')
+        vs_oddrow.append(' ')
+        vs_evenrow.append('|')
+
 
 # construct nested lists of all vertical stitches
-for y in range(HEIGHT):
-    if y % 2 == 0:
-        vgrid.append(veven)
+for row in range(HEIGHT):
+    if row % 2 == 0:
+        vs_grid.append(vs_evenrow)
     else:
-        vgrid.append(vodd)
+        vs_grid.append(vs_oddrow)
 
 # construct horizontal stitches
-while len(hodd) < WIDTH:
-    hodd.append('__')
-    hodd.append('  ')
+while len(hs_oddrow) < WIDTH:
+    hs_oddrow.append('__')
+    hs_oddrow.append('  ')
 
-while len(heven) < WIDTH:
-    heven.append('  ')
-    heven.append('__')
+while len(hs_evenrow) < WIDTH:
+    hs_evenrow.append('  ')
+    hs_evenrow.append('__')
 
-# construct nest lists of all horizontal stitches
-for y in range(HEIGHT):
+# construct nested lists of all horizontal stitches
+for row in range(HEIGHT):
     if random.randint(0, 1) == 0:
-        hgrid.append(heven)
+        hs_grid.append(hs_evenrow)
     else:
-        hgrid.append(hodd)
+        hs_grid.append(hs_oddrow)
 
 # print out the horizontal and vertical stitches together
 print("___"*WIDTH)
-for v, h in zip(vgrid, hgrid):
-    for i in range(WIDTH):
-        print(v[i]+h[i], end='')
+for vs, hs in zip(vs_grid, hs_grid):
+    for stitch in range(WIDTH):
+        print(vs[stitch]+hs[stitch], end='')
     print()
 print("___"*WIDTH)
